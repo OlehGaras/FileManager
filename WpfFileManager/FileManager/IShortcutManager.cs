d6f3bc256f3951aea7e34a1cd5b18e39c0dbca1f
oleh.harasymchuk <oleh.harasymchuk@eleks.com>
2014-02-11 19:45:02 +0200

    444

---
+++ WpfFileManager/FileManager/IShortcutManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileManager
+{
+    public interface IShortcutManager
+    {
+        List<ShortcutAction> GetActions(); 
+        void AddAction(ShortcutAction action);
+        ShortcutAction GetActionByKey(string key);
+        bool MapAction(string key, ShortcutAction action);
+        void UnMapAction(string key);
+        event EventHandler ActionsChanged;
+    }
+}
